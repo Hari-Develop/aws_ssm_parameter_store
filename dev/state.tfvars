257cd3eae90434f47b836d3b911897fb4260c090
hari <yadalaharinadhareddy01@gmail.com>
2023-05-19 08:54:32 +0530

    file changes

---
+++ dev/state.tfvars
@@ -0,0 +1,3 @@
+bucket = "terraform-72"
+key    = "parameter_store/dev/terraform.tfstate"
+region = "us-east-1"
---
+++ main.tf
@@ -0,0 +1,5 @@
+resource "aws_ssm_parameter" "parameter" {
+  name = "parameter_key"
+  type = "string"
+  value = "keyhrrrooo99000"
+}
