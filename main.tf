891acbe9dd903db753fee9abbe74445a97c7987f
hari <yadalaharinadhareddy01@gmail.com>
2023-05-19 09:26:11 +0530

    file changes

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "aws_ssm_parameter" "parameter" {
   name = "parameter_key"
-  type = "String"
+  type = "SecureString"
+  key_id = "b7b69b50-3bdc-4da8-816c-2f2e1c754777"
   value = "keyvalut"
 }
