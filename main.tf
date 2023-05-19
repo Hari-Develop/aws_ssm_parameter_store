e88b32738d081caa5b96012396db40096603bf61
hari <yadalaharinadhareddy01@gmail.com>
2023-05-19 10:31:00 +0530

    file changes

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
-resource "aws_ssm_parameter" "parameters" {
+resource "aws_ssm_parameter" "parameter" {
   count = length(var.parameter)
   name = var.parameter[count.index].name
   type = "SecureString"
   key_id = "b7b69b50-3bdc-4da8-816c-2f2e1c754777"
   value = var.parameter[count.index].value
+  overwrite   = true
 }
