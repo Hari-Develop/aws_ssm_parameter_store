aae8674c73762734a66fe5e7a9e35d790795f2b5
hari <yadalaharinadhareddy01@gmail.com>
2023-06-15 16:39:36 +0530

    file chmhaes

--- main.tf
+++ main.tf
@@ -3,6 +3,5 @@ resource "aws_ssm_parameter" "parameter" {
   name = var.parameter[count.index].name
   value = var.parameter[count.index].value
   type = "SecureString"
-  key_id = "b7b69b50-3bdc-4da8-816c-2f2e1c754777"
   overwrite   = true
 }
