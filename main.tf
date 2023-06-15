1dccb5da752f7a4d003e6f0b52e428ec2c377d59
hari <yadalaharinadhareddy01@gmail.com>
2023-06-15 17:13:14 +0530

    file chnges

--- main.tf
+++ main.tf
@@ -3,4 +3,5 @@ resource "aws_ssm_parameter" "parameter" {
   name = var.parameter[count.index].name
   value = var.parameter[count.index].value
   type = "SecureString"
+  key_id = "7c7c9941-f247-48e3-87d9-479971eb6ac6"
 }
